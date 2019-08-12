5ceffa8b7f55c7322bb05c7aaa12e28c781385d6
Seef Nasrul Islam Bambang <seef@wowrack.com>
2019-08-12 09:28:35 +0700

    add back

--- resources/views/layouts/auth-master.blade.php
+++ resources/views/layouts/auth-master.blade.php
@@ -21,8 +21,10 @@
         <div class="row">
           <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
             <div class="login-brand" >
-              <img src="{{ asset('assets/img/demolishing.png') }}" alt="logo" width="100" class="shadow-light rounded-circle" style="background-color:white;padding:5px;" >
-            </div>
+              <a href="{{url()}}">
+                <img src="{{ asset('assets/img/demolishing.png') }}" alt="logo" width="100" class="shadow-light rounded-circle" style="background-color:white;padding:5px;" >
+              </a>
+              </div>
             @if(session()->has('info'))
             <div class="alert alert-primary">
                 {{ session()->get('info') }}
