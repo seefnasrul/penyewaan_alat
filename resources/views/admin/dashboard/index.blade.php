d1b607e92f76cedb94995d2f1fa5a32a342923c0
Seef Nasrul Islam Bambang <seef@wowrack.com>
2019-08-12 09:07:52 +0700

    my pc

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -11,6 +11,42 @@ Dashboard
   </div>
 
   <div class="section-body">
+
+      <div class="col-lg-12 col-md-12 col-sm-12">
+          <div class="card card-statistic-2">
+            <div class="card-stats">
+              <div class="card-stats-title">Statistics
+              </div>
+              <div class="card-stats-items">
+                <div class="card-stats-item">
+                  <div class="card-stats-item-count">24</div>
+                  <div class="card-stats-item-label">Pending</div>
+                </div>
+                <div class="card-stats-item">
+                  <div class="card-stats-item-count">12</div>
+                  <div class="card-stats-item-label">Shipping</div>
+                </div>
+                <div class="card-stats-item">
+                  <div class="card-stats-item-count">23</div>
+                  <div class="card-stats-item-label">Completed</div>
+                </div>
+              </div>
+            </div>
+            <div class="card-icon shadow-primary bg-primary">
+              <i class="fas fa-archive"></i>
+            </div>
+            <div class="card-wrap">
+              <div class="card-header">
+                <h4>Total Orders</h4>
+              </div>
+              <div class="card-body">
+                59
+              </div>
+            </div>
+          </div>
+        </div>
+
+
   </div>
 </section>
 @endsection
