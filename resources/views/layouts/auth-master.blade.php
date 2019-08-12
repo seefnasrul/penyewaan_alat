e8e1843fe0270aee54dd0a222efa1c9e4d663e9c
Seef Nasrul Islam Bambang <seef@wowrack.com>
2019-08-12 09:30:11 +0700

    add back

--- resources/views/layouts/auth-master.blade.php
+++ resources/views/layouts/auth-master.blade.php
@@ -21,7 +21,7 @@
         <div class="row">
           <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
             <div class="login-brand" >
-              <a href="{{url()}}">
+              <a href="{{url('/')}}">
                 <img src="{{ asset('assets/img/demolishing.png') }}" alt="logo" width="100" class="shadow-light rounded-circle" style="background-color:white;padding:5px;" >
               </a>
               </div>
